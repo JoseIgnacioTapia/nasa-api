081dcc697cf5468a17441e81cc418c8b706fe49d
JoseIgnacioTapia <josignaciot@gmail.com>
2021-03-19 18:25:55 -0300

    Navigation, Loader

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
+function showContent(page) {
+  window.scrollTo({ top: 0, behavior: "instant" });
+  if (page === "results") {
+    resultNav.classList.remove("hidden");
+    favoritesNav.classList.add("hidden");
+  } else {
+    resultNav.classList.add("hidden");
+    favoritesNav.classList.remove("hidden");
+  }
+  loader.classList.add("hidden");
+}
+
 function createDOMNodes(page) {
   const currentArray =
     page === "results" ? resultsArray : Object.values(favorites);
@@ -76,18 +88,19 @@ function updateDOM(page) {
   // Get favorites from localStorage
   if (localStorage.getItem("nasaFavorites")) {
     favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
-    console.log("favorites from localStorage", favorites);
   }
   imagesContainer.textContent = "";
   createDOMNodes(page);
+  showContent(page);
 }
 
 // Get 10 Images from NASA API
 async function getNasaPictures() {
+  loader.classList.remove("hidden");
   try {
     const response = await fetch(apiURL);
     resultsArray = await response.json();
-    updateDOM("favorites");
+    updateDOM("results");
   } catch (error) {
     // Catch Error Here
   }
@@ -99,7 +112,6 @@ function saveFavorites(itemUrl) {
   resultsArray.forEach((item) => {
     if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
       favorites[itemUrl] = item;
-      console.log(favorites);
       // Show save confirmation for 2 seconds
       saveConfirmed.hidden = false;
       setTimeout(() => {
